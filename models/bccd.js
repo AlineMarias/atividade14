a7bed2fdab4f0695f43f6d49b536eb952e5e8787
Aline Maria <139500744+AlineMarias@users.noreply.github.com>
2024-03-31 21:17:18 -0300

    Update bccd.js

--- models/bccd.js
+++ models/bccd.js
@@ -1,35 +1,33 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection =  mysql.createConnection({
-  host: process.env.BCDD_HOST,
-  user: process.env.BCDD_USER,
-  port: process.env.BCDD_PORT,
-  password: process.env.BCDD_PASSWORD,
-  database: process.env.BCDD_DATABASE
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    port: process.env.port,
+    password: process.env.password,
+    database: process.env.database
 });
 
 connection.connect(err => {
-  if (err) {
-    return console.error('Erro ao conectar ao banco de dados: ' + err);
-  }
-  console.log('Conexão bem-sucedida ao banco de dados');
+    if (err) {
+        return console.error('Erro ao conectar ao banco de dados: ' + err);
+    }
+    console.log('Conexão bem-sucedida ao banco de dados');
 });
 
-function consultar(query, callback) {
-  connection.query(query, (err, resultados) => {
-      if (err) {
-          console.error('Erro ao consultar o banco de dados:', err);
-          callback(err, null);
-          return;
-      }
-      console.log('Resultados da consulta:', resultados); 
-      callback(null, resultados);
-  });
+function consultar(query, params, callback) {
+    connection.query(query, params, (err, resultados) => {
+        if (err) {
+            console.error('Erro ao consultar o banco de dados:', err);
+            callback(err, null);
+            return;
+        }
+        console.log('Resultados da consulta:', resultados);
+        callback(null, resultados);
+    });
 }
 
 module.exports = {
-  consultar, 
-  connection
+    consultar
 };
-
