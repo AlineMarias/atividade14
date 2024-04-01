a674cef03c7ffb5d51cd59143cdb2db08fbe797a
Aline Maria <139500744+AlineMarias@users.noreply.github.com>
2024-03-31 21:19:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -8,10 +8,30 @@ app.use(express.static('public'));
 const routerRaiz = require('./routes/index');
 const routerBuscar = require('./routes/buscar');
 
+app.use(express.urlencoded({ extended: true }));
 app.use('/', routerRaiz);
 app.use('/buscar', routerBuscar);
 
-const PORT = process.env.PORT || 3000;
+app.post('/alugar', (req, res) => {
+    const livroId = req.body.livro_id;
+    res.redirect('/cadastro?livro_id=' + livroId); 
+});
+
+app.get('/cadastro', (req, res) => {
+    const livroId = req.query.livro_id; 
+    res.render('cadastro', { livro: {id:  livroId } } ); 
+});
+
+app.post('/cadastrar', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    res.send('Usuário cadastrado com sucesso!');
+});
+
+app.post('/excluir-cadastro', (req, res) => {
+    res.send('Cadastro excluído com sucesso!');
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
