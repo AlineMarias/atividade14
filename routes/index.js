eac8fe2bf7f38b2939c30bee12c8194848bd2063
Aline Maria <139500744+AlineMarias@users.noreply.github.com>
2024-03-31 21:18:32 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { livro_encontrado: null });
+    res.render('index', { livro_encontrado: null, error: null });
 });
 
 module.exports = router;
